3a8064f910cc06f438ff5cc23ea0011a5f04dac9
Millie Walsh <millie@primeacademy.io>
2017-04-07 10:19:48 -0500

    add clarification to constring assign

--- assignment/1_arrayLoopAndConcat.js
+++ assignment/1_arrayLoopAndConcat.js
@@ -1,6 +1,7 @@
 function concatArrayToString(array) {
   var conString = '';
   // Write code here: Concatentate all of the values in array together using a loop
+  // For example: if the array given is [1, 2, 3, 4] the conString should be '1234'
 
 
   // return concatentated values
