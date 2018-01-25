7adb14a74b348d6c9f7f348a4ca26688236a36ce
devjanaprime <dev@primeacademy.io>
2018-01-25 16:13:09 -0600

    more housekeeping

--- test/1_arrayLoopAndConcat.test.js
+++
@@ -1,8 +0,0 @@
-describe('basemode arrayLoopAndConcat.js', function() {
-  describe('concatArrayToString()', function() {
-    it('concatentates the values of the array together', function() {
-      chai.expect(concatArrayToString([1, 2, 3, 4])).to.equal('1234');
-      chai.expect(concatArrayToString([1, 'mohawk', 3, 'four'])).to.equal('1mohawk3four');
-    });
-  });
-});
